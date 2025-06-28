eebd98bc24177196e5e5f222a513312f5d7ae018
ivan <ivanmarti.alves@gmail.com>
2025-06-28 07:42:50 -0300

    feat: implement PATCH endpoint to update checked status of purchase items

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:9000"], 
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"],
     allow_headers=["*"],
     expose_headers=["*"],
     max_age=3600,
